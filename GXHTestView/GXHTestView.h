202b7c5f7ad5423058c3d12aa883a88a3a704716
guanxuhang1234 <18611817195@163.com>
2016-11-09 17:09:53 +0800

    Release 1.0.0.

---
+++ GXHTestView/GXHTestView.h
@@ -0,0 +1,13 @@
+//
+//  GXHTextVIew.h
+//  GXHTextVIewDemo
+//
+//  Created by guanxuhang1234 on 16/11/9.
+//  Copyright © 2016年 liQingGongZuoShi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GXHTestView : UIView
+
+@end
---
+++ GXHTestView/GXHTestView.m
@@ -0,0 +1,27 @@
+//
+//  GXHTextVIew.m
+//  GXHTextVIewDemo
+//
+//  Created by guanxuhang1234 on 16/11/9.
+//  Copyright © 2016年 liQingGongZuoShi. All rights reserved.
+//
+
+#import "GXHTestView.h"
+
+@implementation GXHTestView
+- (instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        NSLog(@"hello world!");
+    }
+    return self;
+}
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
